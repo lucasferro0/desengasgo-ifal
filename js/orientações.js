621060ae0da0b26ee0daa1caee9e46d64ec9e168
Paulo Clebson <pauloclebson270993@gmail.com>
2024-02-14 21:56:23 -0300

    implementação da pagina de orientação

--- js/orientações.js
+++ js/orientações.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const redirectVoltar = function() {
-    window.location.href = "index.html";
-  };
+  const botaoVoltar = document.querySelector("button");
 
-  const botaoVoltar = document.querySelector(".botaoVoltar");
   if (botaoVoltar) {
-      botaoVoltar.addEventListener("click", redirectVoltar);
+    botaoVoltar.addEventListener("click", function () {
+      window.history.back();
+    });
   }
 });
