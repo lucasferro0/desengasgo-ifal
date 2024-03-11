76596ec4afc5eaa965a77336c9786150427ae69e
Claudson <claudsonlima1@outlook.com>
2024-03-10 23:36:44 -0300

    feat: adiciona página da segunda etapa

---
+++ js/etapa-2.js
@@ -0,0 +1,40 @@
+window.localStorage.setItem("errosCometidos", 0)
+
+const imagens = ["../img/posicao_bebe_1.png", "../img/posicao_bebe_2.png", "../img/posicao_bebe_3.png", "../img/tapotagem_movimento_1.png"]
+const indiceImagemCorreta = imagens.length - 1
+const imagemAtual = document.getElementById("posicao-bebe")
+const proximaPagina = "../views/etapa-3.html"
+const telaInicial = "../views/index.html"
+let indiceAtual = 0
+
+function mudarImagem(){
+    if (indiceAtual < imagens.length - 1){
+        indiceAtual++
+    } else {
+        indiceAtual = 0
+    }
+    imagemAtual.src = imagens[indiceAtual]
+}
+
+function contarErro(){
+    let valorAtual = parseInt(window.localStorage.getItem("errosCometidos"))
+    window.localStorage.setItem("errosCometidos", valorAtual + 1)
+}
+
+function sair(){
+    window.location.href = telaInicial
+}
+
+function redirecionarPagina(){
+    window.location.href = proximaPagina
+}
+
+function verificarResposta() {
+    if (indiceAtual === indiceImagemCorreta){
+        alert("Correto!")
+        redirecionarPagina()
+    } else {
+        alert("Errado!")
+        contarErro()
+    }
+}
