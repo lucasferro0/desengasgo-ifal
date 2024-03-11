93a390ed90216e2d149991b733a1abc2ecdfc52d
Claudson <claudsonlima1@outlook.com>
2024-03-10 22:21:41 -0300

    feat: adiciona página da primeira etapa

---
+++ js/etapa-1.js
@@ -0,0 +1,10 @@
+const proximaPagina = "../views/etapa-2.html"
+const telaInicial = "../views/index.html"
+
+function avancar(){
+    window.location.href = proximaPagina
+}
+
+function sair(){
+    window.location.href = telaInicial
+}
