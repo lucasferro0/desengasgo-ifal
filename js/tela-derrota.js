1d30e601088dc2376f3b5a91c3be812e2246a6c5
Claudson <claudsonlima1@outlook.com>
2024-03-13 13:51:26 -0300

    refactor: modifica a página de derrota

--- js/tela-derrota.js
+++ js/tela-derrota.js
@@ -1,36 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const botaoVoltar = document.querySelector(".button-voltar");
-  const botaoContinuar = document.querySelector(".button-vamos");
-  const botaoAjuda = document.querySelector(".button-nao-sei");
+const telaInicial = "../views/etapa-1.html"
+const nomeUsuario = localStorage.getItem("username");
 
-  if (botaoVoltar) {
-    botaoVoltar.addEventListener("click", function () {
-      window.history.back();
-    });
-  }
+document.getElementById("nomeUsuario").textContent = nomeUsuario;
 
-  if (botaoContinuar) {
-    botaoContinuar.addEventListener("click", function () {
-      window.location.href = "tela2.html";
-    });
-  }
-
-  if (botaoAjuda) {
-    botaoAjuda.addEventListener("click", function () {
-      window.location.href = "orientacoes.html";
-    });
-  }
-
-  // Recupera o nome de login do Local Storage
-  var nomeLogin = localStorage.getItem("username");
-  console.log("Nome de login recuperado do Local Storage:", nomeLogin);
-
-  // Verifica se o nome de login foi armazenado
-  if (nomeLogin) {
-    // Define o nome de login no elemento HTML
-    document.getElementById("nomeUsuario").textContent = nomeLogin;
-    console.log("Nome de login definido no elemento HTML:", nomeLogin);
-  } else {
-    console.log("Nome de login não encontrado no Local Storage.");
-  }
-});
+function redirecionarTelaInicial(){
+  window.location.href = telaInicial;
+}
