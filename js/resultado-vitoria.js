4b2138e76dbfd3b89061dd1b6e534f3fb35ff97e
Claudson <claudsonlima1@outlook.com>
2024-03-13 13:20:16 -0300

    refactor: modifica a página de vitória

---
+++ js/resultado-vitoria.js
@@ -0,0 +1,8 @@
+const telaInicial = "../views/index.html"
+const nomeUsuario = localStorage.getItem("username")
+
+document.getElementById("nomeUsuario").textContent = nomeUsuario
+
+function redirecionarTelaInicial(){
+    window.location.href = telaInicial
+}
